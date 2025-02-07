54fd3ec5de2fa1c68033136b7d7429e4abf142c3
Danyss09 <danielacaceresp4@gmail.com>
2025-02-07 15:14:27 -0500

    Update restaurantController.js

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -1,26 +1,13 @@
 // controllers/restaurantController.js
 const Restaurant = require('../models/restaurant');
-const Counter = require('../models/counter');  // Importar el modelo del contador
 
 exports.createRestaurant = async (req, res) => {
   try {
     const { name, address, phone } = req.body;
 
-    // Obtener e incrementar el contador para restaurantes
-    const counter = await Counter.findByIdAndUpdate(
-      { _id: 'restaurantId' },
-      { $inc: { sequence_value: 1 } }, // Incrementar en 1
-      { new: true, upsert: true }      // Crear si no existe
-    );
-
-    const newRestaurant = new Restaurant({
-      _id: counter.sequence_value,    // Asignar el ID incrementado
-      name,
-      address,
-      phone
-    });
-
+    const newRestaurant = new Restaurant({ name, address, phone });
     await newRestaurant.save();
+
     res.status(201).json(newRestaurant);
   } catch (error) {
     res.status(400).json({ error: error.message });
